7831a2ff452fafd53900a67d321603933f3fa2e8
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2021-12-24 20:36:15 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let tglMenu = document.querySelector(".landin-page .header .toggle-lins")
+let ULlinks = document.querySelector(".landin-page .header .links")
+
+tglMenu.onclick = function(e){
+    e.stopPropagation();
+    this.classList.toggle("toggle")
+    ULlinks.classList.toggle("open")
+}
+ULlinks.onclick = (e)=>{
+    e.stopPropagation()
+}   
+// close if clicked in the body
+    document.addEventListener("click",(e)=>{
+    if (e.target !==tglMenu && e.target !==ULlinks){
+        if(ULlinks.classList.contains("open")){
+            tglMenu.classList.remove("toggle")
+            ULlinks.classList.remove("open")            
+        }
+    }
+    
+});
